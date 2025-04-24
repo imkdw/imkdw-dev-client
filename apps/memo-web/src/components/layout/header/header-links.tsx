805ffd3b480ff3b5ccd2013058c1cff12e7866c3
dongwoo <imkdw@kakao.com>
2025-04-24 12:53:15 +0900

    feat: 헤더 버튼에 깃허브 링크 추가

--- apps/memo-web/src/components/layout/header/header-links.tsx
+++ apps/memo-web/src/components/layout/header/header-links.tsx
@@ -1,20 +1,31 @@
+'use client';
+
 import { Moon } from 'lucide-react';
 import { ReactNode } from 'react';
 import GithubIcon from '@/public/images/github.svg';
 import Image from 'next/image';
 
-function HeaderLink({ children }: { children: ReactNode }) {
-  return <button className='flex items-center p-2 rounded-lg bg-gray-100'>{children}</button>;
+function HeaderLink({ children, onClick }: { children: ReactNode; onClick: () => void }) {
+  return (
+    <button className="flex items-center p-2 rounded-lg bg-gray-100" onClick={onClick}>
+      {children}
+    </button>
+  );
 }
 
 export function HeaderLinks() {
   return (
-    <div className='flex items-center gap-2 rounded-lg bg-white p-4 shadow-primary'>
-      <HeaderLink>
-        <Image src={GithubIcon} alt='Github' width={24} height={24} />
+    <div className="flex items-center gap-2 rounded-lg bg-white p-4 shadow-primary">
+      <HeaderLink
+        onClick={() => window.open('https://github.com/imkdw/imkdw-dev-client', '_blank', 'noopener,noreferrer')}
+      >
+        <Image src={GithubIcon} alt="Github" width={24} height={24} />
       </HeaderLink>
-      <HeaderLink>
-        <Moon className='text-gray-600' />
+
+      {/* TODO: 다국어 처리 */}
+      {/* TODO: 다크모드 지원 */}
+      <HeaderLink onClick={() => alert('지원 예정인 기능입니다')}>
+        <Moon className="text-gray-600" />
       </HeaderLink>
     </div>
   );
