2510dad546448297704bfdcc4a7383dd659f3add
dongwoo <imkdw@kakao.com>
2025-05-04 16:38:03 +0900

    feat: 선택한 메모에 대한 사이드바 메모 목록 강조효과 추가

--- apps/memo-web/src/app/[locale]/memo/[slug]/page.tsx
+++ apps/memo-web/src/app/[locale]/memo/[slug]/page.tsx
@@ -3,16 +3,27 @@ import { MemoDetail } from '@/src/components/memo/memo-detail';
 import { getMemo } from '@imkdw-dev-client/api-client';
 import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: '메모 상세 | IMKDW Dev',
-  description: '메모 상세 페이지입니다.',
-};
-
-interface MemoDetailPageProps {
+interface Props {
   params: Promise<{ slug: string }>;
 }
 
-export default async function MemoDetailPage({ params }: MemoDetailPageProps) {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = await params;
+
+  const memo = await getMemo(slug);
+
+  return {
+    title: memo.name,
+    openGraph: {
+      // TODO: 임시 이미지 제거
+      images: '/images/angelic-buster.webp',
+      url: `https://imkdw-dev.com/memo/${slug}`,
+      type: 'website',
+    },
+  };
+}
+
+export default async function MemoDetailPage({ params }: Props) {
   const { slug } = await params;
 
   const memo = await getMemo(slug);
--- apps/memo-web/src/components/layout/sidebar/content/sidebar-content-memo.tsx
+++ apps/memo-web/src/components/layout/sidebar/content/sidebar-content-memo.tsx
@@ -1,4 +1,4 @@
-import { Link } from '@imkdw-dev-client/i18n';
+import { Link, usePathname } from '@imkdw-dev-client/i18n';
 import { cn } from '@imkdw-dev-client/utils';
 import { File } from 'lucide-react';
 
@@ -9,19 +9,23 @@ interface Props {
 }
 
 export function SidebarContentMemo({ level = 0, memoName, slug }: Props) {
+  const pathname = usePathname();
+  const currentSlug = pathname.split('/').pop();
+  const isSelected = currentSlug === slug;
+
   return (
     <li>
       <Link
         href={`/memo/${slug}`}
         type='button'
         className={cn(
-          'flex items-center p-1 w-full cursor-pointer hover:bg-[#3B3B3C] rounded',
+          'flex items-center p-1 w-full cursor-pointer rounded',
           'text-base text-gray-300 hover:text-white',
-          // isFileActive(item.memoId) && 'bg-[#3B3B3C] text-white font-medium',
+          isSelected ? 'bg-[#3B3B3C] text-white' : 'hover:bg-[#3B3B3C]',
         )}
         style={{ paddingLeft: `${level * 16 + 8}px` }}
       >
-        <span className='mr-1 text-gray-400 w-4' />
+        <span className='text-gray-400 w-4' />
         <File size={16} className={cn('mr-2 text-gray-400')} />
         <span>{memoName}</span>
       </Link>
