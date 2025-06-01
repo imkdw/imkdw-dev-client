e40752562793b1f6b07bde709e0cae3df3f33b52
dongwoo <imkdw@kakao.com>
2025-06-01 20:21:54 +0900

    fix: 이미지 업로드시 엑박현상 수정

--- apps/memo-web/src/components/editor/milkdown-editor.tsx
+++ apps/memo-web/src/components/editor/milkdown-editor.tsx
@@ -1,6 +1,7 @@
 import { defaultKeymap } from '@codemirror/commands';
 import { languages } from '@codemirror/language-data';
 import { oneDark } from '@codemirror/theme-one-dark';
+import { upload, uploadConfig, Uploader } from '@milkdown/kit/plugin/upload';
 import { keymap } from '@codemirror/view';
 import { Editor, defaultValueCtx, editorCtx, editorViewOptionsCtx, rootCtx } from '@milkdown/kit/core';
 import { commonmark } from '@milkdown/kit/preset/commonmark';
@@ -9,11 +10,13 @@ import { Milkdown, useEditor } from '@milkdown/react';
 import { nord } from '@milkdown/theme-nord';
 import '@imkdw-dev-client/ui/editor.css';
 import { codeBlockComponent, codeBlockConfig } from '@milkdown/kit/component/code-block';
-import { listener } from '@milkdown/kit/plugin/listener';
-import { listenerCtx } from '@milkdown/kit/plugin/listener';
+import { listener, listenerCtx } from '@milkdown/kit/plugin/listener';
+import { imageBlockComponent } from '@milkdown/kit/component/image-block';
 import { clipboard } from '@milkdown/kit/plugin/clipboard';
 import { getHTML } from '@milkdown/kit/utils';
 import { basicSetup } from 'codemirror';
+import { useImageUpload } from '@imkdw-dev-client/hooks';
+import { Node } from '@milkdown/kit/prose/model';
 
 interface Props {
   content: string;
@@ -22,12 +25,51 @@ interface Props {
   onUploadImage(imageName: string): void;
 }
 
-export function MilkdownEditor({ content, isEditable, onChangeContent }: Props) {
+export function MilkdownEditor({ content, isEditable, onChangeContent, onUploadImage }: Props) {
+  const { uploadImage } = useImageUpload();
+
+  const uploader: Uploader = async (files, schema) => {
+    const images: File[] = [];
+
+    for (let i = 0; i < files.length; i++) {
+      const file = files.item(i);
+      if (!file) {
+        continue;
+      }
+
+      if (!file.type.includes('image')) {
+        continue;
+      }
+
+      images.push(file);
+    }
+
+    const nodes: Node[] = await Promise.all(
+      images.map(async (image) => {
+        const src = await uploadImage(image);
+        const alt = image.name;
+        onUploadImage(src);
+        return schema.nodes.image?.createAndFill({
+          src,
+          alt,
+        }) as Node;
+      }),
+    );
+
+    return nodes.filter((node) => node !== null);
+  };
+
   useEditor((root) =>
     Editor.make()
       .config(nord)
       .config((ctx) => ctx.set(defaultValueCtx, content))
       .config((ctx) => ctx.set(rootCtx, root))
+      .config((ctx) => {
+        ctx.update(uploadConfig.key, (prev) => ({
+          ...prev,
+          uploader,
+        }));
+      })
       .config((ctx) =>
         ctx.update(editorViewOptionsCtx, (prev) => ({
           ...prev,
@@ -55,9 +97,12 @@ export function MilkdownEditor({ content, isEditable, onChangeContent }: Props) 
           }
         });
       })
+
       .use(listener)
       .use(commonmark)
+      .use(imageBlockComponent)
       .use(gfm)
+      .use(upload)
       .use(clipboard)
       .use(codeBlockComponent),
   );
