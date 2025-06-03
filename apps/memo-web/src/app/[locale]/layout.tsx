e191b99ffc2d28deb5705bd4df96cc98ddda9721
imkdw <imkdw@kakao.com>
2025-06-03 16:41:10 +0900

    feat: 토스트 메세지 적용 & 메모 저장시 토스트 출력

--- apps/memo-web/src/app/[locale]/layout.tsx
+++ apps/memo-web/src/app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import { mapleFont } from '@imkdw-dev-client/fonts';
 import { Metadata } from 'next';
 import './globals.css';
+import '@imkdw-dev-client/ui/toast.css';
 import { Header } from '@/src/components/layout/header';
 import { Sidebar } from '@/src/components/layout/sidebar';
 import { SupportedLocale, routing } from '@imkdw-dev-client/i18n';
@@ -11,6 +12,7 @@ import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { AuthInitializer } from '../../components/auth/auth-initializer';
+import { Toaster } from 'sonner';
 
 interface Props {
   children: React.ReactNode;
@@ -43,6 +45,20 @@ export default async function RootLayout({ children, params }: Props) {
           </main>
           {process.env.NEXT_PUBLIC_APP_ENV === 'production' && <Analytics />}
           {process.env.NEXT_PUBLIC_APP_ENV === 'production' && <SpeedInsights />}
+          <Toaster
+            richColors
+            theme='dark'
+            toastOptions={{
+              unstyled: true,
+              classNames: {
+                toast: 'sonner-toast-vscode-style',
+                success: 'sonner-toast-vscode-style sonner-icon-success',
+                error: 'sonner-toast-vscode-style sonner-icon-error',
+                info: 'sonner-toast-vscode-style sonner-icon-info',
+                warning: 'sonner-toast-vscode-style sonner-icon-warning',
+              },
+            }}
+          />
         </body>
       </NextIntlClientProvider>
     </html>
